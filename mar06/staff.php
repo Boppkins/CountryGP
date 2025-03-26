06aead9edbe73a4a2b6ed770997c2f13695bd188
Boppkins <b00160560@mytudublin.ie>
2025-03-26 09:35:50 +0000

    Mar06 - sotwaredev
    Iteration from shared OneDrive. Contains files from 6th of March.

---
+++ mar06/staff.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Staff Country GP</title>
+    <link rel="stylesheet" href="css.css">
+</head>
+<body>
+
+<?php require 'lib/functions.php'; ?>
+<?php require "layouts/header.php"; ?>
+
+<main>
+    <h2>Staff List</h2>
+    <?php 
+    $employees = get_employees();
+    foreach ($employees as $employee) : ?>
+    <div class="employee-card">
+        <div class="employee-info">
+            <p><b><?php echo htmlspecialchars($employee['name']); ?></b></p>
+            <p>Age: <?php echo htmlspecialchars($employee['age']); ?></p>
+            <p>Hire Date: <?php echo htmlspecialchars($employee['hireDate']); ?></p>
+            <p>University: <?php echo htmlspecialchars($employee['uni']); ?></p>
+        </div>
+    </div>
+    <?php endforeach; ?>
+</main>
+    <?php require "layouts/footer.php"; ?>
+
+</body>
+</html>
